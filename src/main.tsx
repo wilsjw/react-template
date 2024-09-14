7357afa15d876bd404d1d2838f23019c40fa1d0c
wilsjw <wilsjw2@student.op.ac.nz>
2024-09-14 18:58:29 +1200

    feat: Removed unnecessary code from template

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import App from './App.tsx';
 import './index.css';
 
-export const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
